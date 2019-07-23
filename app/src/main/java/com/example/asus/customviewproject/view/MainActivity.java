81d86a9c2285e25268180a5bfb1e0dba344b6f5f
rain9155 <jianyu9155@gmail.com>
2019-07-23 15:27:19 +0800

    SwitchButton移除

--- app/src/main/java/com/example/asus/customviewproject/view/MainActivity.java
+++ app/src/main/java/com/example/asus/customviewproject/view/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.asus.customviewproject.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.asus.customviewproject.R;
 
